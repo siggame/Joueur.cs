ced7a7eb332960916b96e0113dd889e09ab050f3
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-02-20 19:46:10 -0600

    Convert all line endings and add .gitattributes to enforce them

--- Joueur/ServerMessages/InvalidData.cs
+++ Joueur/ServerMessages/InvalidData.cs
@@ -1,4 +1,4 @@
-﻿#pragma warning disable 0649 // this is an interopt class and it will never be directly assigned to, Json.Net will instead dynamically use these properties
+#pragma warning disable 0649 // this is an interopt class and it will never be directly assigned to, Json.Net will instead dynamically use these properties
 using System;
 using System.Collections.Generic;
 using System.Linq;
