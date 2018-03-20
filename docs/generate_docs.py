050fed6a6ae0f2f8af8891e7945cfcf104cdb28b
Jacob Fischer <jacob.t.fischer@gmail.com>
2018-03-20 18:33:36 -0500

    Update docs to generate via travis

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,7 +1,6 @@
 import os
 import os.path
 import subprocess
-import argparse
 import shutil
 from datetime import date
 import sys
@@ -11,46 +10,30 @@ def run(*args, **kwargs):
     if error_code != 0: # an error happened
         sys.exit(error_code)
 
-parser = argparse.ArgumentParser(description='Runs the c++ client doc generation script.')
-parser.add_argument('game', action='store', help='the name of the game you want to document. Must exist in ../games/')
-
-args = parser.parse_args()
-
-game_name = args.game[0].upper() + args.game[1:]
-lower_game_name = game_name[0].lower() + game_name[1:]
-
-
-with open("./Doxyfile", "r") as f:
-    base_doxyfile = f.read()
-
-new_doxyfile = base_doxyfile.replace("$$$GAME_NAME$$$", game_name)
-
-temp_path = "./temp"
+temp_path = "./temp/"
 if os.path.isdir(temp_path):
     shutil.rmtree(temp_path)
 os.makedirs(temp_path)
 
-with open(os.path.join(temp_path, "Doxyfile"), "w+") as f:
-    f.write(new_doxyfile)
+with open("../README.md", "r") as file:
+    readme = file.read()
 
-game_path = os.path.join("..", "Games", game_name)
-only_files = [f for f in os.listdir(game_path) if os.path.isfile(os.path.join(game_path, f))]
+game_names = [f for f in os.listdir("../Games") if os.path.isdir(os.path.join("../Games", f))]
 
-for name in only_files:
-    shutil.copyfile(os.path.join(game_path, name), os.path.join(temp_path, name))
+games_sections = '##Games\n\n' + (
+    '\n'.join(["* [{0}](namespaceJoueur_1_1cs_1_1Games_1_1{0}.html)".format(n) for n in game_names])
++ "\n\n")
 
+insert_at = readme.find("## How to Run")
 
-shutil.copyfile(os.path.join("..", "Joueur", "BaseGameObject.cs"), os.path.join(temp_path, "BaseGameObject.cs"))
+with open(os.path.join(temp_path, "README.md"), 'w+') as file:
+    file.write(readme[:insert_at] + games_sections + readme[insert_at:])
 
-with open("../README.md", "r") as f:
-    readme = f.read()
+shutil.copyfile("./Doxyfile", os.path.join(temp_path, "Doxyfile"))
 
-readme = readme.replace("GAME_NAME", game_name).replace("game_name", lower_game_name)
+shutil.copytree("../Games/", os.path.join(temp_path, "Games/"))
 
-with open(os.path.join(temp_path, "README.md"), "w+") as f:
-    f.write(readme)
-
-#shutil.copyfile(os.path.join("..", "README.md"), os.path.join(temp_path, "README.md"))
+shutil.copyfile("../Joueur/BaseGameObject.cs", os.path.join(temp_path, "BaseGameObject.cs"))
 
 run(["doxygen"], shell=True, cwd=temp_path)
 
