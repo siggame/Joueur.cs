6aa3fa15ac1981baf3646594b4a06073e88da78f
Jzaustin295 <Jzaustin295@gmail.com>
2019-10-09 13:25:31 -0500

    added files for Joueur.cs for necrowar

---
+++ Games/Necrowar/tJob.cs
@@ -0,0 +1,105 @@
+// Information about a tower's job/type.
+
+// DO NOT MODIFY THIS FILE
+// Never try to directly create an instance of this class, or modify its member variables.
+// Instead, you should only be reading its variables and calling its functions.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// <<-- Creer-Merge: usings -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+// you can add additional using(s) here
+// <<-- /Creer-Merge: usings -->>
+
+namespace Joueur.cs.Games.Necrowar
+{
+    /// <summary>
+    /// Information about a tower's job/type.
+    /// </summary>
+    public class tJob : Necrowar.GameObject
+    {
+        #region Properties
+        /// <summary>
+        /// Whether this tower type hits all of the units on a tile (true) or one at a time (false).
+        /// </summary>
+        public bool AllUnits { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against abominations.
+        /// </summary>
+        public int DamageVsAbomination { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against ghouls.
+        /// </summary>
+        public int DamageVsGhoul { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against horseman.
+        /// </summary>
+        public int DamageVsHorseman { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against hounds.
+        /// </summary>
+        public int DamageVsHound { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against wraiths.
+        /// </summary>
+        public int DamageVsWraith { get; protected set; }
+
+        /// <summary>
+        /// The amount of damage this type does per attack against zombies.
+        /// </summary>
+        public int DamageVsZombie { get; protected set; }
+
+        /// <summary>
+        /// How much does this type cost in gold.
+        /// </summary>
+        public int GoldCost { get; protected set; }
+
+        /// <summary>
+        /// The amount of starting health this type has.
+        /// </summary>
+        public int Health { get; protected set; }
+
+        /// <summary>
+        /// How much does this type cost in mana.
+        /// </summary>
+        public int ManaCost { get; protected set; }
+
+        /// <summary>
+        /// The number of tiles this type can attack from.
+        /// </summary>
+        public int Range { get; protected set; }
+
+        /// <summary>
+        /// The type title. 'arrow', 'aoe', 'ballista', or 'cleansing'.
+        /// </summary>
+        public string Title { get; protected set; }
+
+
+        // <<-- Creer-Merge: properties -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+        // you can add additional properties(s) here. None of them will be tracked or updated by the server.
+        // <<-- /Creer-Merge: properties -->>
+        #endregion
+
+
+        #region Methods
+        /// <summary>
+        /// Creates a new instance of tJob. Used during game initialization, do not call directly.
+        /// </summary>
+        protected tJob() : base()
+        {
+        }
+
+
+
+        // <<-- Creer-Merge: methods -->> - Code you add between this comment and the end comment will be preserved between Creer re-runs.
+        // you can add additional method(s) here.
+        // <<-- /Creer-Merge: methods -->>
+        #endregion
+    }
+}
